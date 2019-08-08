fb32be94dd026d437ed3db6b9f389d8bcae4dc0b
kjalali21 <50310672+kjalali21@users.noreply.github.com>
2019-08-08 08:46:43 -0700

    tkinter UI

---
+++ tkinter.py
@@ -0,0 +1,37 @@
+import tkinter
+from tkinter import *
+
+
+
+class ParentWindow(Frame):
+    def __init__ (self, master):
+        Frame.__init__ (self)
+
+        self.master = master
+        self.master.resizable(width=False, height=False)
+        self.master.geometry('{}x{}'.format(700, 400))
+        self.master.title('Learning Tkinter!')
+        self.master.config(bg='#000')
+
+        self.varFName = StringVar()
+        self.varLName = StringVar()
+
+
+        self.lblFName = Label(self.master,text="First Name", font = ('Helvetica', 16), fg='black', bg='lightblue')
+        self.lblFName.grid(row=0, column=0)
+
+        self.txtLName = Entry(self.master,text="Last Name", font =("Helvetica", 16), fg='black', bg='lightblue')
+        self.txtLName.grid(row=1, column=0)
+
+        self.txtFName = Entry(self.master,text=self.varFName, font =("Helvetica", 16), fg='black', bg='lightblue')
+        self.txtLName.grid(row=, column=)
+
+        self.txtLName = Entry(self.master,text=self.varLName, font =("Helvetica", 16), fg='black', bg='lightblue')
+        self.txtLName.grid(row=, column=)
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    App = ParentWindow(root)
+    root.mainloop()
